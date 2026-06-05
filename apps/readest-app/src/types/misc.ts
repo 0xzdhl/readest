267db9cc553753d2d370e5258de4e7be55a05c7f
0xzdhl <266611332+0xzdhl@users.noreply.github.com>
2026-06-05 15:00:28 +0800

    refactor(domain): move misc types to domain/misc

--- apps/readest-app/src/types/misc.ts
+++ apps/readest-app/src/types/misc.ts
@@ -1,11 +1,1 @@
-export type Insets = {
-  top: number;
-  bottom: number;
-  left: number;
-  right: number;
-};
-
-export type LocaleWithTextInfo = Intl.Locale & {
-  getTextInfo?: () => { direction: string };
-  textInfo?: { direction: string };
-};
+export * from '@/domain/misc';
